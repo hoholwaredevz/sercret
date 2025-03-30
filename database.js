b4041e46c5b8120f1fcd03d6663ef6eb9146a116
Bluefire Softworks <iarepetsky@outlook.com>
2025-03-30 14:02:17 +0300

    Update database.js

--- database.js
+++ database.js
@@ -1,16 +1,3 @@
 {
-    "users": {
-        "key_123": {
-            "discordNickname": "unixetp.dev",
-            "uid": "0",
-            "hwid": "HWID-ABC123",
-            "expiryDate": "2025-12-31"
-        },
-        "key_321": {
-            "discordNickname": "User2#5678",
-            "uid": "1",
-            "hwid": "HWID-XYZ789",
-            "expiryDate": "2025-06-30"
-        }
-    }
+    "users": {}
 }
